c92691a8caf670a71b774fd325ee9088a07b2952
Sargis <sargis.simonyan1992@gmail.com>
2019-07-20 23:22:02 +0400

    make it better

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('./dist/facebook-photos'));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname,'./dist/facebook-photos/index.html' ))
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {AuthService, SocialUser} from 'angularx-social-login';
+import { FacebookLoginProvider} from 'angularx-social-login';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'facebook-photos';
+export class AppComponent implements  OnInit {
+  title = 'facebook-photo-viewer';
+  private user: SocialUser;
+  private loggedIn: boolean;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      console.log('user', user);
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
+
+  signInWithFB(): void {
+    console.log('sign in')
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
 }
