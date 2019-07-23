01f2ffb82ca9b03e7a8bd73812c97fc1f8d3c548
Sargis <sargis.simonyan1992@gmail.com>
2019-07-23 13:11:42 +0400

    facebook integration code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 
-import {AuthService, SocialUser} from 'angularx-social-login';
-import { FacebookLoginProvider} from 'angularx-social-login';
-import {Observable} from "rxjs/index";
 declare var FB: any;
 
 @Component({
@@ -10,39 +7,41 @@ declare var FB: any;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements  OnInit {
+export class AppComponent implements OnInit {
   title = 'facebook-photo-viewer';
 
-  //private user: SocialUser;
   loggedIn: boolean;
-  userImages =  [];
+  userImages = [];
   errorMessage: string;
+  isLoading = false;
 
-  constructor(private authService: AuthService, private cd: ChangeDetectorRef) { }
+  constructor(private cd: ChangeDetectorRef) {
+  }
 
   ngOnInit() {
-    // this.authService.authState.subscribe((user) => {
-    //   console.log('user', user);
-    //   this.user = user;
-    //   this.loggedIn = (user != null);
-    // });
+    this.initFcbSdk();
+  }
 
+  initFcbSdk() {
     (window as any).fbAsyncInit = () => {
       FB.init({
-        appId      : '344181733165289',
-        cookie     : true,
-        xfbml      : true,
-        version    : 'v3.3'
+        appId: '344181733165289',
+        cookie: true,
+        xfbml: true,
+        version: 'v3.3'
       });
       FB.AppEvents.logPageView();
     };
 
-    (function(d, s, id) {
+    (function (d, s, id) {
       var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {return; }
-      js = d.createElement(s); js.id = id;
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
       js.src = 'https://connect.facebook.net/en_US/sdk.js';
-      fjs.parentNode.insertBefore(js,  fjs);
+      fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
   }
 
@@ -50,45 +49,44 @@ export class AppComponent implements  OnInit {
   submitLogin() {
     console.log('submit login to facebook');
     FB.login((response) => {
-      console.log(response);
       if (response.authResponse && response.status === 'connected') {
+        this.isLoading = false;
+        this.loggedIn = true;
         this.getUserPhotos(response.authResponse.userID);
       } else {
-	    this.loggedIn = false;
+        this.loggedIn = false;
         console.log('User login failed');
       }
     });
-	
+
 
   }
 
   getUserPhotos(id): void {
     // v3.3/me?fields=albums.fields(photos.fields(source))
-    FB.api(`v3.3/me?fields=albums.fields(photos.fields(source))`,  (response) => {
-      console.log(response);
-      if (response && !response.error && response.albums) {
-		    this.loggedIn = true;
-            let user_iamges = [];
-            const {albums} = response;
-            for (const item of albums.data) {
-              if (item.photos) {
-                user_iamges = [...user_iamges, ...item.photos.data];
-              }
+    FB.api(`v3.3/me?fields=albums.fields(photos.fields(source))`, (response) => {
+        if (response && !response.error && response.albums) {
+          this.isLoading = true;
+          let userImages = [];
+          const {albums} = response;
+          for (const item of albums.data) {
+            if (item.photos) {
+              userImages = [...userImages, ...item.photos.data];
             }
-        this.userImages = user_iamges;      
-		this.errorMessage = '';
-      } else {
-		  this.loggedIn = false;
-		  this.errorMessage = 'Permission is required';
-	  }
-	  this.cd.detectChanges();
+          }
+          this.userImages = userImages;
+          this.errorMessage = '';
+        } else {
+          this.errorMessage = 'Permission is required';
+        }
+        this.cd.detectChanges();
       }
     );
   }
 
   logout() {
     FB.logout((response) => {
-	this.loggedIn = false;
+      this.loggedIn = false;
       console.log(response);
       // user is now logged out
     });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,40 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {SocialLoginModule, AuthServiceConfig, LoginOpt} from 'angularx-social-login';
-import { FacebookLoginProvider } from 'angularx-social-login';
 
-const fbLoginOptions: LoginOpt = {
-  scope: 'email',
-  return_scopes: true,
-  enable_profile_selector: true
-};
-
-
-const config = new AuthServiceConfig([
-  {
-    id: FacebookLoginProvider.PROVIDER_ID,
-    provider: new FacebookLoginProvider('344181733165289', fbLoginOptions)
-  }
-]);
-
-export function provideConfig() {
-  return config;
-}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    SocialLoginModule
+    BrowserModule
   ],
   providers: [
-    {
-      provide: AuthServiceConfig,
-      useFactory: provideConfig
-    }
   ],
   bootstrap: [AppComponent]
 })
